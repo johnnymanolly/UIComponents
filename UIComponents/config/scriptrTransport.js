ff11ae2c6c4bdb89eb6ac7e80ea88fb8ccd86c63
johnnymanolly <manollyjohnny@gmail.com>
2020-01-26 22:56:16 +0200

    updates

--- UIComponents/config/scriptrTransport.js
+++ UIComponents/config/scriptrTransport.js
@@ -1,10 +1,10 @@
 var wssConfig = ["wsClientProvider",function (wsClientProvider) {
-  wsClientProvider.setToken("QzFBQzlGQTQ4MTpzY3JpcHRyOkY3MkEzRkVBRUMwNzE3RUE5RDk4RjM3MkNDOUI3N0Qx");
-  wsClientProvider.setPublishChannel("requestChannel");
-  wsClientProvider.setSubscribeChannel("responseChannel");
+  wsClientProvider.setToken(config.token);
+  wsClientProvider.setPublishChannel(config.requestChannel);
+  wsClientProvider.setSubscribeChannel(config.responseChannel);
 }];
 
 var httpsConfig = ["httpClientProvider",function (httpClientProvider) {
-  httpClientProvider.setBaseUrl("https://estia.scriptrapps.io");
-  httpClientProvider.setToken("QzFBQzlGQTQ4MTpzY3JpcHRyOkY3MkEzRkVBRUMwNzE3RUE5RDk4RjM3MkNDOUI3N0Qx");
+  httpClientProvider.setBaseUrl(config.domain);
+  httpClientProvider.setToken(config.token);
 }]
